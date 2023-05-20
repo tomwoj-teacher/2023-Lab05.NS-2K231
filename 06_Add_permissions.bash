089ea96c8f2eae2d401d19155976197b4aa0ea1d
manowabn <132829276+manowabn@users.noreply.github.com>
2023-05-20 13:49:44 +0200

    06_Add_permissions.bash

--- 06_Add_permissions.bash
+++ 06_Add_permissions.bash
@@ -1,8 +1,31 @@
 #!/bin/bash
 #Nowak Mateusz
 
-touch file.txt
-ls -l
-chown jan file.txt
-chmod 700 file.txt
-ls -l
+echo "Podaj nazwe katalogu, ktoremu uprawnienia zamierzasz zmienic: "
+read file
+echo "Dla kogo chcesz zmienic upranwienia?"
+echo "1. Dla wlasciciela"
+echo "2. Dla grupy"
+echo "3. Dla reszty"
+read option
+
+case $option in
+	1)
+	echo "Podaj nowe uprawnienia dla wlasciciela (np. rwx): "
+	read uprawnienia
+	chmod u=$uprawnienia $file
+	echo "Uprawnienia zostaly nadane"
+	;;
+	2)
+	echo "Podaj nowe uprawnienia dla grupy (np. rwx): "
+	read uprawnienia
+	chmod g=$uprawnienia $file
+	echo "Uprawnienia zostaly nadane"	
+	;;
+	3)
+	echo "Podaj nowe uprawnienia dla grupy (np. rwx): "
+	read uprawnienia
+	chmod o=$uprawnienia $file
+	echo "Uprawnienia zostaly nadane"
+	;;
+esac
